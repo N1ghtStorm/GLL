6ea3e74a27511db42ee36d26481de3e03d79a4ab
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-18 19:58:11 +0300

    add double linked list

--- src/main.go
+++ src/main.go
@@ -1,11 +1,32 @@
 package main
 
 func main() {
-
+	var dLL = newDoubleLinkedList(1)
+	println(dLL.firstnode.value)
 }
 
 type doubleLinkedList struct {
+	firstnode *dllNode
+}
+
+func newDoubleLinkedList(value int) doubleLinkedList {
+	return doubleLinkedList{firstnode: &dllNode{value: value, next: nil, prev: nil}}
+}
+
+func (dll doubleLinkedList) AddValue(value int) {
+	if dll.firstnode != nil {
+		dll.firstnode.addNext(value)
+	} else {
+		dll.firstnode = &dllNode{value: value, next: nil, prev: nil}
+	}
 }
 
 type dllNode struct {
+	value int
+	next  *dllNode
+	prev  *dllNode
+}
+
+func (node dllNode) addNext(value int) {
+	node.next = &dllNode{value: value, next: nil, prev: nil}
 }
